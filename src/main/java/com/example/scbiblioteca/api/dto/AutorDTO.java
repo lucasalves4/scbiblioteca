8e3d420be8b3cc7e5ba94ef281d1871685399bff
Lucas Alves <lucasalves.12@live.com>
2023-03-22 19:17:20 -0300

    adicionar dto

--- src/main/java/com/example/scbiblioteca/api/dto/AutorDTO.java
+++ src/main/java/com/example/scbiblioteca/api/dto/AutorDTO.java
@@ -1,4 +1,23 @@
 package com.example.scbiblioteca.api.dto;
 
+import com.example.scbiblioteca.model.entity.Autor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AutorDTO {
+
+    private Long id;
+    private String nome;
+
+    public static AutorDTO create(Autor autor) {
+        ModelMapper modelMapper = new ModelMapper();
+        AutorDTO dto = modelMapper.map(autor, AutorDTO.class);
+        dto.nome = autor.getNome();
+        return dto;
+    }
 }
--- src/main/java/com/example/scbiblioteca/api/dto/ConfiguracaoDTO.java
+++ src/main/java/com/example/scbiblioteca/api/dto/ConfiguracaoDTO.java
@@ -1,4 +1,40 @@
 package com.example.scbiblioteca.api.dto;
 
+import com.example.scbiblioteca.model.entity.Configuracao;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfiguracaoDTO {
+
+    private Long id;
+    private String tipoPrazo;
+
+    private String prazoEntregaQuantDias;
+
+    private float valorMulta;
+
+    private boolean permiteRenovar;
+
+    private int quantMaximaEmprestimo;
+
+    private boolean permiteReserva;
+
+    public static ConfiguracaoDTO create(Configuracao configuracao) {
+        ModelMapper modelMapper = new ModelMapper();
+        ConfiguracaoDTO dto = modelMapper.map(configuracao, ConfiguracaoDTO.class);
+        dto.tipoPrazo = configuracao.getTipoPrazo();
+        dto.prazoEntregaQuantDias = configuracao.getPrazoEntregaQuantDias();
+        dto.valorMulta = configuracao.getValorMulta();
+        if (configuracao.isPermiteRenovar()) dto.permiteRenovar = true;
+        else dto.permiteRenovar = false;
+        dto.quantMaximaEmprestimo = configuracao.getQuantMaximaEmprestimo();
+        if (configuracao.isPermiteReserva()) dto.permiteReserva = true;
+        else dto.permiteReserva = false;
+        return dto;
+    }
 }
