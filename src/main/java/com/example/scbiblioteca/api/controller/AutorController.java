68457c161583b880fa3631d12646b6da7aadae17
Amanda Faria <amanda.faria@projetusti.com.br>
2023-04-29 22:06:42 -0300

    develop adicionar post

--- src/main/java/com/example/scbiblioteca/api/controller/AutorController.java
+++ src/main/java/com/example/scbiblioteca/api/controller/AutorController.java
@@ -1,15 +1,14 @@
 package com.example.scbiblioteca.api.controller;
 
 import com.example.scbiblioteca.api.dto.AutorDTO;
+import com.example.scbiblioteca.exception.RegraNegocioException;
 import com.example.scbiblioteca.model.entity.Autor;
 import com.example.scbiblioteca.service.AutorService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,4 +37,38 @@ public class AutorController {
         return ResponseEntity.ok(autor.map(AutorDTO::create));
     }
 
+//    @PostMapping()
+//    public ResponseEntity post(AutorDTO dto) {
+//        try {
+//            Autor autor = converter(dto);
+//            autor = autorService.salvar(autor.getNome());
+//            autor.setNome(endereco);
+//            autor = service.salvar(autor);
+//            return new ResponseEntity(autor, HttpStatus.CREATED);
+//        } catch (RegraNegocioException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
+//
+//
+//    public Autor converter(AutorDTO dto) {
+//        ModelMapper modelMapper = new ModelMapper();
+//        Autor autor = modelMapper.map(dto, Autor.class);
+//        autor.setNome(autor);
+//        if (dto.getNome()!=null){
+//            Optional<Autor> autor = autorService.getAutorById(dto.getId());
+//            if(!id.isPresent)){
+//            autor.setId(null);
+//            }
+//        }
+// //       if (dto.getNome() != null) {
+// //           Optional<Curso> curso = autorService(dto.getIdCurso());
+// //           if (!autor.isPresent()) {
+// //               autor.setNome(null);
+////            } else {
+// //               autor.setNome(curso.get());
+// //           }
+//  //      }
+//        return autor;
+//    }
 }
