c6a37e12ee85da011ef8e7813f69a58df69d22ac
Amanda Faria <amanda.faria@projetusti.com.br>
2023-05-01 15:19:14 -0300

    limpar lixo

--- src/main/java/com/example/scbiblioteca/api/controller/TituloController.java
+++ src/main/java/com/example/scbiblioteca/api/controller/TituloController.java
@@ -1,10 +1,8 @@
 package com.example.scbiblioteca.api.controller;
 
-import com.example.scbiblioteca.api.dto.ConfiguracaoDTO;
 import com.example.scbiblioteca.api.dto.TituloDTO;
 import com.example.scbiblioteca.exception.RegraNegocioException;
 import com.example.scbiblioteca.model.entity.Autor;
-import com.example.scbiblioteca.model.entity.Configuracao;
 import com.example.scbiblioteca.model.entity.Documento;
 import com.example.scbiblioteca.model.entity.Titulo;
 import com.example.scbiblioteca.service.AutorService;
